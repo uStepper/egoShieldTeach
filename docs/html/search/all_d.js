8cdf96a1d3ee87f84dc5f4515328661e490579fb
Thomas Olsen <thomas@parkalot.dk>
2018-03-17 14:17:28 +0100

    Prepared for release of 1.1.0

--- docs/html/search/all_d.js
+++ docs/html/search/all_d.js
@@ -1,4 +1,13 @@
 var searchData=
 [
-  ['u8g2',['u8g2',['../classegoShield.html#adfbe1e4483d6c137bc522f3aebd8a2a7',1,'egoShield']]]
+  ['rec',['rec',['../classegoShield.html#a35bc04ba2d67492023e0a2f4b8f89927',1,'egoShield']]],
+  ['recbt',['RECBT',['../egoShieldTeach_8h.html#acb50064256bed884dcc9df4eb13a3dcd',1,'egoShieldTeach.h']]],
+  ['record',['record',['../classegoShield.html#ab039ce317b79475e1d16846800bef788',1,'egoShield']]],
+  ['recordbtn',['recordBtn',['../classegoShield.html#a28208101f6b44a2bde136d5cf5478c57',1,'egoShield']]],
+  ['recordmode',['recordMode',['../classegoShield.html#a0dccc775a87134bf91cdf3c558013d01',1,'egoShield']]],
+  ['recordpage',['recordPage',['../classegoShield.html#a135a6b4942e467cc28f6251040e64d19',1,'egoShield']]],
+  ['resetallbutton',['resetAllButton',['../classegoShield.html#a456e3364735fa555e3b4032aa8848254',1,'egoShield']]],
+  ['resetbutton',['resetButton',['../classegoShield.html#a865040c72084a9c6be1ffbbe3676100c',1,'egoShield']]],
+  ['resetwdt',['RESETWDT',['../egoShieldTeach_8h.html#a7ce116aee3168eb16d533efd5698075a',1,'egoShieldTeach.h']]],
+  ['resolution',['resolution',['../classegoShield.html#aaa2a2e8ea5b11bafb5c191132575e9af',1,'egoShield']]]
 ];
---
+++ docs/html/search/defines_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['plbt',['PLBT',['../egoShieldTeach_8h.html#a6510637d9b5ec4881698d7951dcf8974',1,'egoShieldTeach.h']]]
+];
